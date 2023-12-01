845859bccd2a68dcf703036df4ff45589c985ab4
noahbros <brosseauna@gmail.com>
2023-11-30 19:03:48 -0500

    Update Frontend
    - Designed the navbar, still isn't done.
    - Linked /login to the web-page, although having some trouble with the navbar (seems like all the CSS you do in App.vue doesn't carry out to any other page)
    - Created a css folder in src/components/css (holding the css properties for App.vue)

--- Frontend/ProjetWebPartie2/src/App.vue
+++ Frontend/ProjetWebPartie2/src/App.vue
@@ -4,81 +4,28 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/login">Login</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <RouterLink to="/" class="navbar-brand" href="#">Navbar</RouterLink>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <RouterLink to="/login" class="nav-link active login" aria-current="page" href="#">Login</RouterLink>
+              <RouterLink to="/about" class="nav-link" href="#">About</RouterLink>
+              <a class="nav-link" href="#">Pricing</a>
+            </div>
+            <RouterView/>
+          </div>
+        </div>
       </nav>
     </div>
   </header>
-
-  <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+<style lang="scss" scoped>
+@import "./components/css/navstyle.css" 
 </style>
